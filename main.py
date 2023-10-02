23dfde1fef12cbc3dc147a11610a928451321d9d
sxuzmeee <144207608+sxuzmeee@users.noreply.github.com>
2023-10-02 14:22:36 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,119 @@
+
+#x = int(input())
+#print(x, x + 1, x + 2, sep='\n')
+
+
+#a = int(input())
+#b = int(input())
+#c = int(input())
+#print(a + b + c)
+
+
+#x = int(input())
+#print('Объем равен', x * x * x)
+#print('Площадь полной поверхности равна', 6 * x * x)
+
+
+#x = int(input())
+#y = int(input())
+#print(3 * (x + y) * (x + y ) * (x + y) + 275 * y * y - 127 * x - 41)
+
+
+#x = int(input())
+#print('Предыдущее число:', x - 1)
+#print('Текущее число:', x)
+#print('Следующее число:', x + 1)
+
+
+
+#a=9900
+#b=55600
+#c=3999
+#d=2990
+#print('Цена трёх компьютеров:', (a + b + c + d) * 3)
+
+
+
+#a = int(input())
+#d = int(input())
+#n = int(input())
+#print(a+d*(n-1))
+
+
+#x=int(input())
+#print(x,'---', 2*x,'---', 3*x,'---', 4*x,'---', 5*x, sep='', end='')
+
+
+#n=int(input())
+#k=int(input())
+#a=k%n
+#b=k//n
+#print('Кол-во мандаринов на школьника:', b,'Остаток мандаринов:', a)
+
+
+
+#a=int(input())
+#print(a//2 + a%2)
+
+
+
+#m = int(input())
+#h = m // 60
+#s = m % 60
+#print(m, "мин - это", h, "час", s, "минут.")
+
+
+
+#num = int(input())
+#a = num % 10
+#b = (num % 100) // 10
+#c = num // 100
+#print("Сумма чисел =", c + b + a)
+#print("Произведение чисел =", c * b * a)
+
+
+
+#n = input()
+#print('   _~_    ' * int(n))
+#print('  (o o)   ' * int(n))
+#print(' /  V  \\  ' * int(n))
+#print('/(  _  )\\ ' * int(n))
+#print('  ^^ ^^   ' * int(n))
+
+
+#abc = int(input())
+#x = 1
+#n = (x // 10 ** k) % 10
+#print(n)
+
+
+
+#a = int(input())
+#h = a % (60 * 24) // 60
+#m = a % 60
+#print(h, m)
+
+
+
+#a = int(input())
+#print(1 - a)
+
+
+
+#a = int(input())
+#print((a//2+1)*2)
+
+
+
+#v = int(input())
+#t = int(input())
+#a = v * t
+#n = a // 109
+#print(-(109 * n - a))
+
+
+
+#a = float(input())
+#b = float(input())
+#c = float(input())
+#print(int(1 + (a - c - 1) / (b - c)))
